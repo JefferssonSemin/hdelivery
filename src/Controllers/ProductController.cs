ad03ce91cffc475db9cc1ea19bd7e5db22b82b4c
JefferssonSemin <jeffesemin@hotmail.com>
2021-07-22 14:24:41 -0300

    chore: add controllers

---
+++ src/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using src.Domain;
+using str.Data;
+
+namespace EFCore.Multitenant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get([FromServices] ApplicationContext db )
+        {
+            var product = db.Products.ToArray();
+
+            return product;
+        }
+    }
+}
---
+++ src/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using src.Domain;
+using str.Data;
+
+namespace EFCore.Multitenant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreController : ControllerBase
+    {
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Store> Get([FromServices] ApplicationContext db )
+        {
+            var store = db.Store.ToArray();
+
+            return store;
+        }
+    }
+}
